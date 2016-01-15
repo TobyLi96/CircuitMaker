e40a57b17ece8d8465ea29d6a5807a8241852e79
TobyUCL <litaolun@hotmail.com>
2016-01-15 13:33:17 +0000

    refined junit test

--- src/circuitmaker/test/CircuitTest.java
+++ src/circuitmaker/test/CircuitTest.java
@@ -1,52 +1,44 @@
 package circuitmaker.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import java.io.File;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.geometry.VPos;
-import javafx.scene.Cursor;
-import javafx.scene.Group;
-import javafx.scene.Scene;
+import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.input.TransferMode;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import javafx.stage.DirectoryChooser;
 
 
 
-public class CircuitTest{
+public class CircuitTest {
+
+	boolean imageEqual(Image image1, Image image2) {
+       PixelReader pr1 = image1.getPixelReader();
+       PixelReader pr2 = image2.getPixelReader();
+       boolean isEqual = true;
+       for (int i = 0; i < 40; i++) {
+           for (int j = 0; j < 40; j++) {
+               if (pr1.getArgb(i, j) != pr2.getArgb(i, j)) {
+                   isEqual = false;
+               }
+           }
+       }
+       return isEqual;
+    }
 	
+	private Node getNodeFromGridPane(GridPane gridPane, int col, int row) {
+        for (Node node : gridPane.getChildren()) {
+            if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
+                return node;
+            }
+        }
+        return null;
+    }
 	
 	@Test
 	public void testImageView() {
@@ -62,23 +54,10 @@ public class CircuitTest{
         boolean testEqual = imageEqual(currentImage.getImage(), new Image(CircuitTest.class.getResourceAsStream("../rsrc/Cell.png")));
        
         
-        assertTrue( testEqual == true) ;
+        assertTrue(testEqual) ;
 	}
 	
 	
-	 boolean imageEqual(Image image1, Image image2) {
-         PixelReader pr1 = image1.getPixelReader();
-         PixelReader pr2 = image2.getPixelReader();
-         boolean isEqual = true;
-         for (int i = 0; i < 40; i++) {
-             for (int j = 0; j < 40; j++) {
-                 if (pr1.getArgb(i, j) != pr2.getArgb(i, j)) {
-                     isEqual = false;
-                 }
-             }
-         }
-         return isEqual;
-     }
 	 
 	 @Test
 	 public void testPane() {
@@ -89,22 +68,54 @@ public class CircuitTest{
         double height = wirePane.getHeight();
         double width = wirePane.getWidth();
         String style =  wirePane.getStyle();
-	    assertTrue( height == 400 && width == 400 && style == "-fx-background-color: #99ccff;") ;
+	    assertTrue(height == 400 && width == 400 && style == "-fx-background-color: #99ccff;") ;
 	}
 	
+	 @Test
+	 public void testDrawLine(){
+		 Line line = new Line();
+		 line.setStartX(0.0);
+		 line.setStartY(0.0);
+		 line.setEndX(100.0);
+		 line.setEndY(100.0);
+		 double startX = line.getStartX();
+		 double startY = line.getStartY();
+		 double endX = line.getEndX();
+		 double endY = line.getEndY();
+		 assertTrue(startX == 0.0 && startY == 0.0 && endX == 100.0 && endY == 100.0);
+	 }
 	
 	@Test
-	public void imageEqual(){
+	public void testImageEqual(){
 		Image image1 = new Image(CircuitTest.class.getResourceAsStream("../rsrc/Cell.png"));
 		Image image2 = new Image(CircuitTest.class.getResourceAsStream("../rsrc/Cell.png"));
 		
 		boolean checkImage = imageEqual(image1, image2);
-		assertTrue( checkImage == true) ;
+		assertTrue(checkImage) ;
 		
 	}
 	
+	@Test
+	public void testCellContent(){
+		GridPane grid = new GridPane();
+		grid.setPrefSize(400, 400);
+		for (int i = 0; i < 10; i++) {
+	        for (int j = 0; j < 10; j++) {
+	            ImageView cell = new ImageView();
+	            cell.setFitWidth(40);
+	            cell.setFitHeight(40);
+	            if(i == 5 && j == 5){
+	            	cell.setImage(new Image(CircuitTest.class.getResourceAsStream("../rsrc/Cell.png")));
+	            }
+	            grid.add(cell, i, j);
+	        }
+	    }
 	
-	
-	
-    
+		ImageView currentImage = (ImageView)getNodeFromGridPane(grid, 5, 5);
+		boolean testEqual = imageEqual(currentImage.getImage(), new Image(CircuitTest.class.getResourceAsStream("../rsrc/Cell.png")));
+		
+		assertTrue(testEqual);
+		
+		
+	}
 }
